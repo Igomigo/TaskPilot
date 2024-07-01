64919860a2ca5c5953f64ab5c1b6adb3bf9abb73
Igomigo <igomigofatai@gmail.com>
2024-07-01 13:23:59 +0100

    Code: job queue created

---
+++ utils/queue.js
@@ -0,0 +1,22 @@
+// Initializes the job processing queue
+
+const queue = require("bull");
+
+// Create the queue
+const TaskQueue = new queue("taskQueue", {
+    redis: {
+        host: "localhost",
+        port: 6379
+    }
+});
+
+// Add events listeners for successful or failed operations
+TaskQueue.on("completed", (job, result) => {
+    console.log(`
+        Job ${job.id} of type ${job.name} completed with result ${result}`);
+});
+
+TaskQueue.on("failed", (job, err) => {
+    console.log(`
+        Job ${job.id} of type ${job.name} failed with error ${err.message}`);
+});
