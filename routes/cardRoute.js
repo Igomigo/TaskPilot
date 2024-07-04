8f15d7c1f820b5908c7a46423a1328852f9e5c1a
Igomigo <igomigofatai@gmail.com>
2024-07-04 13:25:25 +0100

    Update: new routes added to the card route

--- routes/cardRoute.js
+++ routes/cardRoute.js
@@ -12,4 +12,22 @@ const router = express.Router();
  */
 router.post("/:listId/card", authWare, card.createCard);
 
+/**
+ * GET request to retrieve a card data
+ * url: http://localhost:3000/b/cards/:cardId
+ */
+router.get("/cards/:cardId", authWare, card.getCard);
+
+/**
+ * PUT request to update a card data
+ * url: http://localhost:3000/b/:cardId/update
+ */
+router.put("/:cardId/update", authWare, card.updateCard);
+
+/**
+ * DELETE request to delete a card and related data
+ * url: http://localhost:3000/b/:cardId/delete
+ */
+router.delete("/:cardId", authWare, card.deleteCard);
+
 module.exports = router;
