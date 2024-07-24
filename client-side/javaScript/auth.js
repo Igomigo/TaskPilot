096f0f2f62300e9c9f770e8b0fafa8632ef551cc
Igomigo <igomigofatai@gmail.com>
2024-07-24 11:52:30 +0100

    Update: Interface update

--- client-side/javaScript/auth.js
+++ client-side/javaScript/auth.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
-    
+
                 const result = await response.json();
     
                 if (result.status) {
