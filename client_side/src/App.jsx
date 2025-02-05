7811f3b1ef5a079eda864dd19d9db6d29de12ca6
Igomigo <igomigofatai@gmail.com>
2025-02-05 16:55:57 +0100

    Feat: Add network online/offline detection functionality

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -2,8 +2,39 @@ import React, { useEffect } from 'react'
 import './App.css'
 import toast, { Toaster } from 'react-hot-toast';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { MdError } from "react-icons/md";
 
 const App = () => {
+  // State Management
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
+
+  useEffect(() => {
+    // Function to update offLine status
+    const handleNetworkChange = () => {
+      if (!navigator.onLine) {
+        setIsOffline(true);
+      } else {
+        setIsOffline(false);
+      }
+    }
+
+    // Check initial network status
+    if (!navigator.onLine) {
+      setIsOffline(true);
+    }
+
+    // Listen for online/offline network events
+    window.addEventListener("offline", handleNetworkChange);
+    window.addEventListener("online", handleNetworkChange);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("offline", handleNetworkChange);
+      window.removeEventListener("online", handleNetworkChange);
+    };
+
+  }, []);
 
   return (
     <>
@@ -11,6 +42,23 @@ const App = () => {
       <main>
         <Outlet></Outlet>
       </main>
+      {
+        isOffline && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/75 backdrop-blur-sm">
+            <div className="w-full max-w-md bg-white rounded-lg shadow-xl overflow-hidden">
+              <div className="flex items-center justify-center space-x-3 bg-red-50 p-4 border-b border-red-100">
+                <MdError className="text-red-500 flex-shrink-0" size={24} />
+                <h2 className="text-xl font-semibold text-gray-800">Network Connection Error</h2>
+              </div>
+              <div className="p-6">
+                <p className="text-gray-600 text-center">
+                  Please check your internet connection. We can't load this page right now.
+                </p>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </>
   )
 }
--- client_side/src/pages/ActivityLogPage.jsx
+++ client_side/src/pages/ActivityLogPage.jsx
@@ -3,70 +3,106 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 import { FiActivity, FiPlus, FiEdit, FiTrash2, FiMove } from 'react-icons/fi';
+import { useRef } from 'react';
+import useLogout from '../hooks/useLogout';
+import Loading from '../components/loading';
 
 const ActivityLogPage = () => {
-  // State Management
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
   // Hooks
+  const observer = useRef(null);
+  const scrollRef = useRef(null);
+  const handleLogout = useLogout();
   const user = useSelector(state => state?.user);
   const navigate = useNavigate();
 
-  const fetchActivityLogs = async () => {
-    const url = `${import.meta.env.VITE_BACKEND_URL}/b/activity-log`;
+  // State Management
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
-    const token = user?.token;
+  // Infinite scrolling with the observer API
+  const lastActivityRef = (node) => {
+    // Remove any existing observer
+    if (observer.current) observer.current.disconnect();
 
-    if (!token) {
-      console.warn("No token available; redirecting to login.");
-      navigate("/login");
-      return;
-    }
-
-    setLoading(true);
-
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}`
-        }
-      });
-
-      if (response.status === 401) {
-        // Token is invalid or expired, redirect to login
-        toast.error("Session expired, redirecting to login");
-        localStorage.removeItem("token");
-        dispatch(logout());
-        navigate("/login");
-        return;
+    // Create a new observer, an instance of the intersection observer API
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage(prev => prev + 1);
       }
+    });
 
-      if (!response.ok) {
-        throw new Error("An Unknown error occurred");
-      }
-
-      const logs = await response.json();
-
-      setLogs(logs);
-
-    } catch (error) {
-      console.log("Error:", error);
-      toast.error("Failed to fetch activity logs");
-    } finally {
-      setLoading(false);
-    }
-  }
+    // Observe the last activity log for when it comes into the viewport
+    if (node) observer.current.observe(node);
+  } 
 
   useEffect(() => {
-    if (user?.token) {
-      fetchActivityLogs();
-    } else {
-      console.error("User token not available yet");
+    const fetchActivityLogs = async () => {
+      if (!user.token || !hasMore) {
+        return;
+      }
+  
+      const url = `${import.meta.env.VITE_BACKEND_URL}/b/logs/activities?page=${page}&limit=10`;
+  
+      setLoading(true);
+  
+      try {
+        const response = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${user.token}`
+          }
+        });
+  
+        if (response.status === 401) {
+          // Token is invalid or expired, redirect to login
+          toast.error("Session expired, redirecting to login");
+          handleLogout();
+        }
+  
+        if (!response.ok) {
+          console.log(await response.json());
+          throw new Error("An Unknown error occurred");
+        }
+  
+        const activityLogsData = await response.json();
+  
+        setLogs(prev => {
+          const newActivities = [...prev,  ...activityLogsData.activityLogs];
+
+          // Maintain scroll position
+          setTimeout(() => {
+            if (scrollRef.current) {
+              const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
+              // Define a threshold to consider the user "at the bottom"
+              const threshold = 50; // pixels
+              const isAtBottom = scrollHeight - scrollTop - clientHeight < threshold;
+        
+              // Only auto-scroll if the user is already near the bottom
+              if (isAtBottom) {
+                scrollRef.current.scrollTop = scrollHeight - clientHeight;
+              }
+            }
+          }, 0);
+
+          return newActivities;
+        });
+
+        setHasMore(activityLogsData.hasMore);
+  
+      } catch (error) {
+        console.error("Error:", error);
+        toast.error("Failed to fetch activity logs");
+  
+      } finally {
+        setLoading(false);
+      }
     }
-  }, [user]);
+
+    fetchActivityLogs();
+  }, [page, user]);
 
   const getActionIcon = (action) => {
     switch (action) {
@@ -89,17 +125,15 @@ const ActivityLogPage = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div ref={scrollRef} className="container mx-auto px-4 py-8 overflow-y-auto scroll">
       <h1 className="text-3xl font-bold mb-6 text-white">Activity Log</h1>
       {loading ? (
-        <div className="flex justify-center items-center h-64">
-          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-        </div>
+        <Loading />
       ) : (
-        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="bg-white shadow-md rounded-lg">
           <ul className="divide-y divide-gray-200">
-            {logs.map((log) => (
-              <li key={log._id} className="p-4 hover:bg-gray-50 transition duration-150 ease-in-out">
+            {logs?.map((log, index) => (
+              <li key={log._id} ref={index === logs.length - 1 ? lastActivityRef : null} className="p-4 hover:bg-gray-50 transition duration-150 ease-in-out">
                 <div className="flex items-center space-x-4">
                   <div className="flex-shrink-0">
                     {getActionIcon(log.action)}
