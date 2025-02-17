087d35503022e31926657fa06bf3c73911e1106a
Igomigo <igomigofatai@gmail.com>
2025-02-17 17:34:45 +0100

    Feat: add notifications schema

---
+++ server_side/models/notification.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["Personal", "System", "Targeted"],
+        required: true
+    },
+    seen: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
